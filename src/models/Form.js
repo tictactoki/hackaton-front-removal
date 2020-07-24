fca99d7a47f169410bea5930144d1f606ae0a8e0
Stéphane Wong <stephane.wong-ext@groupeseloger.com>
2020-07-24 15:32:49 +0200

    wip

--- src/models/Form.js
+++ src/models/Form.js
@@ -8,19 +8,36 @@ export class Form extends React.Component {
             email: null,
             site: null
         }
+        this.emailChange = this.emailChange(this);
+    }
+
+
+    emailChange(event) {
+        this.setState({email: this.state.email});
     }
 
     render() {
         return (
           <form>
               <label for="email">Email</label>
-              <input type="text" id="email" className="email"/>
+              <input type="text" id="email" onChange={this.emailChange}/>
               <label for="site">Site</label>
-              <select name="Sites">
+              <select name="Site" id="site">
                   <option value="SeLoger">SeLoger</option>
                   <option value="SeLogerNeuf">SeLogerNeuf</option>
                   <option value="BD">Belle Demeures</option>
               </select>
+              <div>
+                <div>
+                    <label for="trust">Je n'ai pas confiance dans le traitement de mes données personnelles</label>
+                    <input type="checkbox" name="trust"/>
+                </div>
+                  <div>
+                    <label for="spam">Je reçois trop d'email/sms</label>
+                    <input type="checkbox" name="spam"/>
+                </div>
+              </div>
+
           </form>
         );
     }
