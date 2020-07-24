5f36e961c2edc8442fef575dd21198a253d60679
Stéphane Wong <stephane.wong-ext@groupeseloger.com>
2020-07-24 16:48:45 +0200

    wip

--- src/models/Form.js
+++ src/models/Form.js
@@ -8,30 +8,28 @@ export class Form extends React.Component {
             email: null,
             site: null
         }
-        this.emailChange = this.emailChange(this);
     }
 
     prepare_query(){
-        const url = "https://ws-gdpr-user-removal.prod.aws.datahub.poliris.net/api/on-demand?bubbles=&mediaPurposes="
-        var website = ""
-        var email = "&email="
-        if (this.state.site != null) website += "&webSite=" + this.state.site
-        if (this.state.site != null) email += this.state.email
-        return url + email + website
+        const url = "https://ws-gdpr-user-removal.prod.aws.datahub.poliris.net/api/on-demand?bubbles=&mediaPurposes=";
+        var website = "";
+        var email = "&email=";
+        if (this.state.site != null) website += "&webSite=" + this.state.site;
+        if (this.state.site != null) email += this.state.email;
+        return url + email + website;
     }
 
     handleSubmit(event){
-        event.preventDefault()
-        this.state.email = event.data.email
-        this.state.site = event.data.site
+        event.preventDefault();
+        this.setState({email: event.data.email, site: event.data.site});
         const requestOptions = {
             method: 'DELETE',
             headers: {
                 "accept": "application/json"
             }
         };
-        const query = this.prepare_query()
-        fetch(query, requestOptions)
+        const query = this.prepare_query();
+        fetch(query, requestOptions);
     }
 
     render() {
@@ -47,7 +45,6 @@ export class Form extends React.Component {
                 </select>
                 <div>
                     {this.props.reasons.map(function(reason, ind) {
-                        console.log(reason);
                         return <div>
                             <label for={reason.name}>{reason.value}</label>
                             <input type="checkbox" name={reason.name}/>
